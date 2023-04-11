d481f537b6ec06bda7705cbd5f801484bbe19910
Leslie <Kooliesk@gmail.com>
2023-04-11 18:51:00 +0200

    Add styling

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 const Rocket = ({ rocket }) => (
   <div className="Rocket-Card">
-    <h2>{rocket.name}</h2>
-    <p>{rocket.description}</p>
     <div className="Rocket-Images">
       {rocket.flickr_images && rocket.flickr_images.length > 0 && (
       <img src={rocket.flickr_images[0]} alt={`Rocket ${rocket.name}`} />
       )}
+    </div>
+    <div>
+      <h2>{rocket.name}</h2>
+      <p>{rocket.description}</p>
     </div>
   </div>
 );
